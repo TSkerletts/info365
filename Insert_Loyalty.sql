64bcab4c6c51431c5564c6f2e2b34195616afa53
TSkerletts <tgs48@drexel.edu>
2016-11-14 21:53:33 -0500

    Adding insert statements for RESERVATION, TRIP, and LOYALTY_PROGRAM tables

--- Insert_Loyalty.sql
+++ Insert_Loyalty.sql
@@ -1,1 +1,22 @@
 --Insert Statements for Loyalty_Program Table
+
+INSERT INTO LOYALTY_PROGRAM VALUES (40001, 00001, 'Prefers driver 10007', '4 door car', 500, 'Extra 100 points per trip');
+INSERT INTO LOYALTY_PROGRAM VALUES (40002, 00002, 'Prefers driver 10003', '2 door car', 1000, '$10 off next ride');
+INSERT INTO LOYALTY_PROGRAM VALUES (40003, 00003, 'Prefers driver 10001', 'SUV', 200, 'None');
+INSERT INTO LOYALTY_PROGRAM VALUES (40004, 00004, 'Prefers driver 10009', '4 door car', 3000, 'None');
+INSERT INTO LOYALTY_PROGRAM VALUES (40005, 00005, 'Prefers driver 10020', '2 door car', 900, '$10 off next ride');
+INSERT INTO LOYALTY_PROGRAM VALUES (40006, 00006, 'Prefers driver 10006', '2 door car', 700, 'None');
+INSERT INTO LOYALTY_PROGRAM VALUES (40007, 00007, 'Prefers driver 10018', '4 door car', 1200, 'Extra 100 points per trip');
+INSERT INTO LOYALTY_PROGRAM VALUES (40008, 00008, 'Prefers driver 10019', 'SUV', 2100, '$10 off next ride');
+INSERT INTO LOYALTY_PROGRAM VALUES (40009, 00009, 'Prefers driver 10014', '4 door car', 6000, 'None');
+INSERT INTO LOYALTY_PROGRAM VALUES (40010, 00010, 'Prefers driver 10003', '2 door car', 5400, 'None');
+INSERT INTO LOYALTY_PROGRAM VALUES (40011, 00011, 'Prefers driver 10002', '4 door car', 1600, 'None');
+INSERT INTO LOYALTY_PROGRAM VALUES (40012, 00012, 'Prefers driver 10012', '4 door car', 3700, 'Extra 100 points per trip');
+INSERT INTO LOYALTY_PROGRAM VALUES (40013, 00013, 'Prefers driver 10004', 'SUV', 100, '$10 off next ride');
+INSERT INTO LOYALTY_PROGRAM VALUES (40014, 00014, 'Prefers driver 10001', '4 door car', 400, 'None');
+INSERT INTO LOYALTY_PROGRAM VALUES (40015, 00015, 'Prefers driver 10005', '4 door car', 600, 'Extra 100 points per trip');
+INSERT INTO LOYALTY_PROGRAM VALUES (40016, 00016, 'Prefers driver 10017', '2 door car', 1900, '$10 off next ride');
+INSERT INTO LOYALTY_PROGRAM VALUES (40017, 00017, 'Prefers driver 10011', 'SUV', 2700, 'Extra 100 points per trip');
+INSERT INTO LOYALTY_PROGRAM VALUES (40018, 00018, 'Prefers driver 10008', '4 door car', 3600, 'None');
+INSERT INTO LOYALTY_PROGRAM VALUES (40019, 00019, 'Prefers driver 10018', '2 door car', 5000, 'Extra 100 points per trip');
+INSERT INTO LOYALTY_PROGRAM VALUES (40020, 00020, 'Prefers driver 10020', '2 door car', 0, '$10 off next ride');
